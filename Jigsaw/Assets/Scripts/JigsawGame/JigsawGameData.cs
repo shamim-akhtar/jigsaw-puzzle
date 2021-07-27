d115093f325a6102b563f712f7f1a165eb9f1533
Shamim Akhtar <shamim.akhtar@gmail.com>
2021-07-27 08:40:37 +0800

    Added a duration for showing ads

--- Jigsaw/Assets/Scripts/JigsawGame/JigsawGameData.cs
+++ Jigsaw/Assets/Scripts/JigsawGame/JigsawGameData.cs
@@ -7,151 +7,152 @@ using System.Runtime.Serialization;
 
 public class JigsawGameData : Singleton<JigsawGameData>
 {
-    ImageMetaData[] mImageDataList;
-    public bool mMetaDataLoaded = false;
+  ImageMetaData[] mImageDataList;
+  public bool mMetaDataLoaded = false;
 
-    private int mIndex = 0;
+  private int mIndex = 0;
+  public float mLastAdShowTime = 0.0f;
 
-    List<ImageMetaData> mFilteredImages = new List<ImageMetaData>();
+  List<ImageMetaData> mFilteredImages = new List<ImageMetaData>();
 
 
-    public void NextImage()
+  public void NextImage()
+  {
+    mIndex++;
+    if (mIndex >= mFilteredImages.Count)
     {
-        mIndex++;
-        if (mIndex >= mFilteredImages.Count)
+      mIndex = 0;
+    }
+  }
+
+  public void PreviousImage()
+  {
+    --mIndex;
+    if (mIndex < 0)
+    {
+      mIndex = mFilteredImages.Count - 1;
+    }
+  }
+
+  public string GetImageFilename()
+  {
+    return mFilteredImages[mIndex].filename;
+  }
+
+  public ImageMetaData GetCurrentImageData()
+  {
+    if (mFilteredImages.Count > 0)
+    {
+      return mFilteredImages[mIndex];
+    }
+    return null;
+  }
+
+  public void SetCurrentImageDataStatus(ImageMetaData.Status status)
+  {
+    ImageMetaData data = mFilteredImages[mIndex];
+    data.status = status;
+  }
+
+  public int GetFilteredImageCount()
+  {
+    return mFilteredImages.Count;
+  }
+
+  private void Start()
+  {
+    LoadMetaData();
+    mMetaDataLoaded = true;
+  }
+
+  IEnumerator Coroutine_AutoSave()
+  {
+    while (true)
+    {
+      yield return new WaitForSeconds(15.0f);
+      SaveMetaData();
+    }
+  }
+
+  private bool LoadMetaData()
+  {
+    string filename = Application.persistentDataPath + "/metadata.json";
+    if (!File.Exists(filename))
+    {
+      filename = "metadata";
+      Debug.Log(filename);
+      TextAsset targetFile = Resources.Load<TextAsset>(filename);
+      mImageDataList = JsonHelper.FromJson<ImageMetaData>(targetFile.text);
+    }
+    else
+    {
+      StreamReader inStream = File.OpenText(filename);
+      string jsonString = inStream.ReadToEnd();
+      inStream.Close();
+      mImageDataList = JsonHelper.FromJson<ImageMetaData>(jsonString);
+
+      if (mImageDataList.Length == 0)
+      {
+        TextAsset targetFile = Resources.Load<TextAsset>("metadata");
+        mImageDataList = JsonHelper.FromJson<ImageMetaData>(targetFile.text);
+      }
+    }
+
+    return true;
+  }
+
+  public void SaveMetaData()
+  {
+    string filename = Application.persistentDataPath + "/metadata.json";
+
+    string jsonString = JsonHelper.ToJson(mImageDataList, true);
+
+    StreamWriter outStream = File.CreateText(filename);
+    outStream.Write(jsonString);
+    outStream.Close();
+  }
+
+  public void SetFilter(ImageMetaData.Status status)
+  {
+    mFilteredImages.Clear();
+    mIndex = 0;
+    switch (status)
+    {
+      case ImageMetaData.Status.COMPLETED:
         {
-            mIndex = 0;
-        }
-    }
-
-    public void PreviousImage()
-    {
-        --mIndex;
-        if(mIndex < 0)
-        {
-            mIndex = mFilteredImages.Count- 1;
-        }
-    }
-
-    public string GetImageFilename()
-    {
-        return mFilteredImages[mIndex].filename;
-    }
-
-    public ImageMetaData GetCurrentImageData()
-    {
-        if(mFilteredImages.Count > 0)
-        {
-            return mFilteredImages[mIndex];
-        }
-        return null;
-    }
-
-    public void SetCurrentImageDataStatus(ImageMetaData.Status status)
-    {
-        ImageMetaData data = mFilteredImages[mIndex];
-        data.status = status;
-    }
-
-    public int GetFilteredImageCount()
-    {
-        return mFilteredImages.Count;
-    }
-
-    private void Start()
-    {
-        LoadMetaData();
-        mMetaDataLoaded = true;
-    }
-
-    IEnumerator Coroutine_AutoSave()
-    {
-        while(true)
-        {
-            yield return new WaitForSeconds(15.0f);
-            SaveMetaData();
-        }
-    }
-
-    private bool LoadMetaData()
-    {
-        string filename = Application.persistentDataPath + "/metadata.json";
-        if (!File.Exists(filename))
-        {
-            filename = "metadata";
-            Debug.Log(filename);
-            TextAsset targetFile = Resources.Load<TextAsset>(filename);
-            mImageDataList = JsonHelper.FromJson<ImageMetaData>(targetFile.text);
-        }
-        else
-        {
-            StreamReader inStream = File.OpenText(filename);
-            string jsonString = inStream.ReadToEnd();
-            inStream.Close();
-            mImageDataList = JsonHelper.FromJson<ImageMetaData>(jsonString);
-
-            if(mImageDataList.Length == 0)
+          for (int i = 0; i < mImageDataList.Length; ++i)
+          {
+            if (mImageDataList[i].status == ImageMetaData.Status.COMPLETED)
             {
-                TextAsset targetFile = Resources.Load<TextAsset>("metadata");
-                mImageDataList = JsonHelper.FromJson<ImageMetaData>(targetFile.text);
+              mFilteredImages.Add(mImageDataList[i]);
             }
+          }
+          break;
         }
-
-        return true;
-    }
-
-    public void SaveMetaData()
-    {
-        string filename = Application.persistentDataPath + "/metadata.json";
-
-        string jsonString = JsonHelper.ToJson(mImageDataList, true);
-
-        StreamWriter outStream = File.CreateText(filename);
-        outStream.Write(jsonString);
-        outStream.Close();
-    }
-
-    public void SetFilter(ImageMetaData.Status status)
-    {
-        mFilteredImages.Clear();
-        mIndex = 0;
-        switch (status)
+      case ImageMetaData.Status.STARTED:
         {
-            case ImageMetaData.Status.COMPLETED:
-                {
-                    for(int i = 0; i <mImageDataList.Length; ++i)
-                    {
-                        if(mImageDataList[i].status == ImageMetaData.Status.COMPLETED)
-                        {
-                            mFilteredImages.Add(mImageDataList[i]);
-                        }
-                    }
-                    break;
-                }
-            case ImageMetaData.Status.STARTED:
-                {
-                    for (int i = 0; i < mImageDataList.Length; ++i)
-                    {
-                        if (mImageDataList[i].status == ImageMetaData.Status.STARTED)
-                        {
-                            mFilteredImages.Add(mImageDataList[i]);
-                        }
-                    }
-                    break;
-                }
-            default:
-                {
-                    mFilteredImages.AddRange(mImageDataList);
-                    break;
-                }
+          for (int i = 0; i < mImageDataList.Length; ++i)
+          {
+            if (mImageDataList[i].status == ImageMetaData.Status.STARTED)
+            {
+              mFilteredImages.Add(mImageDataList[i]);
+            }
+          }
+          break;
         }
-    }
-
-    void OnApplicationPause(bool flag)
-    {
-        if (flag)
+      default:
         {
-            SaveMetaData();
+          mFilteredImages.AddRange(mImageDataList);
+          break;
         }
     }
+  }
+
+  void OnApplicationPause(bool flag)
+  {
+    if (flag)
+    {
+      SaveMetaData();
+    }
+  }
 }
--- Jigsaw/Assets/Scripts/JigsawGame/Menu.cs
+++ Jigsaw/Assets/Scripts/JigsawGame/Menu.cs
@@ -7,149 +7,163 @@ using UnityEngine.Advertisements;
 
 public class Menu : MonoBehaviour
 {
-    public CameraMovement CameraMovement;
-    public Button BtnHome;
-    public Button BtnPrev;
-    public FixedButton BtnZoomIn;
-    public Button BtnReset;
-    public FixedButton BtnZoomOut;
-    public FixedButton BtnHint;
+  public CameraMovement CameraMovement;
+  public Button BtnHome;
+  public Button BtnPrev;
+  public FixedButton BtnZoomIn;
+  public Button BtnReset;
+  public FixedButton BtnZoomOut;
+  public FixedButton BtnHint;
 
-    public Button BtnPlay;
-    public Button BtnNext;
+  public Button BtnPlay;
+  public Button BtnNext;
 
-    public Text TextTotalTiles;
-    public Text TextTilesInPlace;
-    public Text TextTime;
+  public Text TextTotalTiles;
+  public Text TextTilesInPlace;
+  public Text TextTime;
 
-    public Text TextWin;
+  public Text TextWin;
 
-    // Our game controls when the menu is enabled of disabled.
-    // Enabled = false means that the UI won't handle
-    // inputs.
-    static public bool Enabled { get; set; } = true;
+  // Our game controls when the menu is enabled of disabled.
+  // Enabled = false means that the UI won't handle
+  // inputs.
+  static public bool Enabled { get; set; } = true;
 
-    public delegate void DelegateOnClick();
-    public DelegateOnClick OnClickHome;
-    public DelegateOnClick OnClickPlay;
-    public DelegateOnClick OnClickNext;
+  public delegate void DelegateOnClick();
+  public DelegateOnClick OnClickHome;
+  public DelegateOnClick OnClickPlay;
+  public DelegateOnClick OnClickNext;
 
-    public AudioSource mAudioSource;
-    public AudioClip mBtnClickAudio;
+  public AudioSource mAudioSource;
+  public AudioClip mBtnClickAudio;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    GameApp.Instance.mAds.onAdFinish += OnUnityAdsDidFinish;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (!Enabled) return;
+
+    if (BtnZoomIn.Pressed)
     {
-        GameApp.Instance.mAds.onAdFinish += OnUnityAdsDidFinish;
+      CameraMovement.ZoomIn();
     }
 
-    // Update is called once per frame
-    void Update()
+    if (BtnZoomOut.Pressed)
     {
-        if (!Enabled) return;
-
-        if (BtnZoomIn.Pressed)
-        {
-            CameraMovement.ZoomIn();
-        }
-
-        if (BtnZoomOut.Pressed)
-        {
-            CameraMovement.ZoomOut();
-        }
+      CameraMovement.ZoomOut();
     }
+  }
 
-    public void SetTotalTiles(int count)
-    {
-        TextTotalTiles.text = count.ToString();
-    }
+  public void SetTotalTiles(int count)
+  {
+    TextTotalTiles.text = count.ToString();
+  }
 
-    public void SetTilesInPlace(int count)
-    {
-        TextTilesInPlace.text = count.ToString();
-    }
+  public void SetTilesInPlace(int count)
+  {
+    TextTilesInPlace.text = count.ToString();
+  }
 
-    public void SetTimeInSeconds(double tt)
-    {
-        System.TimeSpan t = System.TimeSpan.FromSeconds(tt);
+  public void SetTimeInSeconds(double tt)
+  {
+    System.TimeSpan t = System.TimeSpan.FromSeconds(tt);
 
-        string time = string.Format("{0:D2}:{1:D2}:{2:D2}",
-                        t.Hours,
-                        t.Minutes,
-                        t.Seconds);
+    string time = string.Format("{0:D2}:{1:D2}:{2:D2}",
+                    t.Hours,
+                    t.Minutes,
+                    t.Seconds);
 
-        TextTime.text = time;
-    }
+    TextTime.text = time;
+  }
 
-    public void OnClickBtnHome()
-    {
+  public float mDurationAdShow = 60.0f;
+  public void OnClickBtnHome()
+  {
 
 #if (UNITY_ANDROID || UNITY_IOS)
-        if (Advertisement.isInitialized)
-        {
-            GameApp.Instance.mAds.ShowInterstitialAd();
-        }
-        else
-        {
-            FadeSceneLoader.Instance.FadeSceneLoad("JigsawImageSelection");
-        }
+    float currentTime = Time.time;
+    bool showAd = false;
+    if ((currentTime - JigsawGameData.Instance.mLastAdShowTime) > 
+      mDurationAdShow)
+    {
+      showAd = true;
+    }
+    Debug.Log("Show ads: " + showAd + ", Duration: " + 
+      (currentTime - JigsawGameData.Instance.mLastAdShowTime) + 
+      ", mLastAdShowTime: " + 
+      JigsawGameData.Instance.mLastAdShowTime);
+    if (Advertisement.isInitialized && showAd)
+    {
+      JigsawGameData.Instance.mLastAdShowTime = Time.time;
+      GameApp.Instance.mAds.ShowInterstitialAd();
+    }
+    else
+    {
+      FadeSceneLoader.Instance.FadeSceneLoad("JigsawImageSelection");
+    }
 #else
-        FadeSceneLoader.Instance.FadeSceneLoad("JigsawImageSelection");
+    FadeSceneLoader.Instance.FadeSceneLoad("JigsawImageSelection");
 #endif
-    }
+  }
 
-    public void OnClickBtnPlay()
-    {
-        mAudioSource.PlayOneShot(mBtnClickAudio);
-        OnClickPlay?.Invoke();
-    }
+  public void OnClickBtnPlay()
+  {
+    mAudioSource.PlayOneShot(mBtnClickAudio);
+    OnClickPlay?.Invoke();
+  }
 
-    public void OnClickBtnNext()
-    {
-        mAudioSource.PlayOneShot(mBtnClickAudio);
+
+  public void OnClickBtnNext()
+  {
+    mAudioSource.PlayOneShot(mBtnClickAudio);
 #if (UNITY_ANDROID || UNITY_IOS)
-        if (Advertisement.isInitialized)
-        {
-            GameApp.Instance.mAds.ShowInterstitialAd();
-        }
-        else
-        {
-            FadeSceneLoader.Instance.FadeSceneLoad("JigsawImageSelection");
-        }
+    if (Advertisement.isInitialized)
+    {
+      GameApp.Instance.mAds.ShowInterstitialAd();
+    }
+    else
+    {
+      FadeSceneLoader.Instance.FadeSceneLoad("JigsawImageSelection");
+    }
 #endif
-    }
+  }
 
-    public void SetActivePlayBtn(bool flag)
+  public void SetActivePlayBtn(bool flag)
+  {
+    mAudioSource.PlayOneShot(mBtnClickAudio);
+
+    BtnPlay.gameObject.SetActive(flag);
+    //BtnNext.gameObject.SetActive(flag);
+    BtnPrev.gameObject.SetActive(flag);
+
+    BtnHome.gameObject.SetActive(!flag);
+    BtnZoomIn.gameObject.SetActive(!flag);
+    BtnReset.gameObject.SetActive(!flag);
+    BtnZoomOut.gameObject.SetActive(!flag);
+    BtnHint.gameObject.SetActive(!flag);
+  }
+
+  public void OnUnityAdsDidFinish(string surfacingId, ShowResult showResult)
+  {
+    // Define conditional logic for each ad completion status:
+    if (showResult == ShowResult.Finished)
     {
-        mAudioSource.PlayOneShot(mBtnClickAudio);
-
-        BtnPlay.gameObject.SetActive(flag);
-        //BtnNext.gameObject.SetActive(flag);
-        BtnPrev.gameObject.SetActive(flag);
-
-        BtnHome.gameObject.SetActive(!flag);
-        BtnZoomIn.gameObject.SetActive(!flag);
-        BtnReset.gameObject.SetActive(!flag);
-        BtnZoomOut.gameObject.SetActive(!flag);
-        BtnHint.gameObject.SetActive(!flag);
+      // Reward the user for watching the ad to completion.
     }
-
-    public void OnUnityAdsDidFinish(string surfacingId, ShowResult showResult)
+    else if (showResult == ShowResult.Skipped)
     {
-        // Define conditional logic for each ad completion status:
-        if (showResult == ShowResult.Finished)
-        {
-            // Reward the user for watching the ad to completion.
-        }
-        else if (showResult == ShowResult.Skipped)
-        {
-            // Do not reward the user for skipping the ad.
-        }
-        else if (showResult == ShowResult.Failed)
-        {
-            //Debug.LogWarning(“The ad did not finish due to an error.”);
-        }
-        FadeSceneLoader.Instance.FadeSceneLoad("JigsawImageSelection");
-        //SceneManager.LoadScene("JigsawImageSelection");
+      // Do not reward the user for skipping the ad.
     }
+    else if (showResult == ShowResult.Failed)
+    {
+      //Debug.LogWarning(“The ad did not finish due to an error.”);
+    }
+    FadeSceneLoader.Instance.FadeSceneLoad("JigsawImageSelection");
+    //SceneManager.LoadScene("JigsawImageSelection");
+  }
 }
